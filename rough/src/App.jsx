501d1ffb65120d798339ed10f047bbc4645cb18b
K-L-SRUJAN-PRO <klsrujan.1614@gmail.com>
2026-02-04 15:32:07 +0530

    two way binding

--- rough/src/App.jsx
+++ rough/src/App.jsx
@@ -1,5 +1,5 @@
-// import React from 'react'
-// import { useState } from 'react'
+import React from 'react'
+import { useState } from 'react'
 
 
 // const App = () => {
@@ -31,21 +31,75 @@
 
 // practicing object operation
 
-import { React, useState } from 'react'
+// import { React, useState } from 'react'
 
+
+// const App = () => {
+//   var c=20
+//   const [n, setn] = useState({ name: "srujan", age: 22 })
+
+//   function btn(){
+//     setn(perv=>({...perv,age:20}))
+//   }
+//   return (
+//     <>
+//       <div>{n.name},{n.age}</div>
+//       <button onClick={btn}>click me to change age</button>
+//     </>
+//   )
+// }
+
+// export default App
+
+//perventing webpage from reloading after each submission
+// const App = () => {
+//   const subs = (e) => {
+//     e.preventDefault()
+//     console.log("from submitted")
+//   }
+//   return (
+//     <div>
+//       <form onSubmit={(e)=>{
+//         subs(e)
+//       }}>
+//         <input type="text" placeholder='Enter your name'/>
+//         <button>submit</button>
+//       </form>
+//     </div>
+//   )
+// }
+
+// export default App
+
+//interacting with input field via DOM
+
+// import React from 'react'
 
 const App = () => {
-  var c=20
-  const [n, setn] = useState({ name: "srujan", age: 22 })
-
-  function btn(){
-    setn(perv=>({...perv,age:20}))
+  const btn = (e)=>{
+    e.preventDefault()
+    // console.log("from submited");
+    
   }
+  const [Text, setText] = useState('')
   return (
-    <>
-      <div>{n.name},{n.age}</div>
-      <button onClick={btn}>click me to change age</button>
-    </>
+    <div>
+      <form onSubmit={(e)=>{
+        btn(e)
+        console.log("form subitted by ",Text)
+      }}>
+        <input 
+        type="text" 
+        placeholder='Enter yout text' 
+        value={Text} 
+        onChange={(e)=>{
+          setText(e.target.value)
+          console.log(e.target.value)
+        }}
+          />
+        <button >Submit</button>
+      </form>
+    </div>
   )
 }
 
